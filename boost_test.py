055f6953183806c3432b084a8b74e5581d86ac2e
eg <y.ge1222@gmail.com>
2018-06-07 18:09:19 +0200

    xgboost single model for whole range

--- boost_test.py
+++ boost_test.py
@@ -8,6 +8,8 @@ import pickle
 from reactor_ode_delta import ignite_post
 import pandas as pd
 
+#from sklearn.model_selection import
+
 
 # test=ode_o.drop('N2',axis=1)
 def sp_plot(species, models, do, do_1):
@@ -43,22 +45,25 @@ def sp_plot_gpu(species, models, do, do_1):
 
 
 def sp_plot_gpu_mask(species, models, do, do_1,mask_pred):
-    test = do_1[mask_pred][species]
-    dtest = xgb.DMatrix(do)
-    # sp_pred = models[species].predict(dtest)
-    sp_pred = np.exp(models[species].predict(dtest))
-    # sp_pred=np.ma.masked_where(mask_pred,sp_pred)
-    sp_pred = sp_pred[mask_pred]
-
     plt.figure()
-    plt.subplot(1, 2, 1)
-    plt.plot(test.index,test)
-    plt.plot(test.index,sp_pred, 'rd',ms=2)
-    plt.title(species + ':' + str(r2_score(test, sp_pred)))
+    for state in {'b:','u:'}:
+        mask_pred=~mask_pred
+        test = do_1[mask_pred][species]
+        dtest = xgb.DMatrix(do)
+        # sp_pred = models[species].predict(dtest)
+        sp_pred = np.exp(models[state+species].predict(dtest))
+        # sp_pred=np.ma.masked_where(mask_pred,sp_pred)
+        sp_pred = sp_pred[mask_pred]
 
-    plt.subplot(1, 2, 2)
-    plt.plot((test - sp_pred) / test)
-    plt.ylim(-0.1, 0.1)
+
+        plt.subplot(1, 2, 1)
+        plt.plot(test.index,test)
+        plt.plot(test.index,sp_pred, 'rd',ms=2)
+        plt.title(species + ':' + str(r2_score(test, sp_pred)))
+
+        plt.subplot(1, 2, 2)
+        plt.plot((test - sp_pred) / test)
+        plt.ylim(-0.1, 0.1)
     plt.show()
 
 
@@ -92,40 +97,42 @@ tot = df_x[df_x.columns.drop(['T', 'dt'])]
 tot.head()
 tot.sum(axis=1)
 
-bstReg_gpu = {}
+
 # for sp in df_y.columns.drop('dt'):
 # mask=df_x['H2O2']>1e-7
 # mask=df_x['H2O2']<1e-7
 # mask = df_x['O2'] < 0.01
 mask = (df_x['O2'] < 0.01) | (df_x['H2'] < 0.01)
-mask = ~mask
-for sp in ['T']:
-    #     dtrain=xgb.DMatrix(df_x.loc[mask],label=df_y.loc[mask][sp]*10e10)
-    # target = df_y.loc[mask][sp]
-    target=np.log(df_y.loc[mask][sp])
-    dtrain = xgb.DMatrix(df_x.loc[mask], label=target)
-    param = {
-        # 'ntree_limit':200,
-        'max_depth': 10,
-        'eta': 0.3,
-        'silent': 1,
-        'predictor': 'gpu_predictor',
-        'objective': 'gpu:reg:linear'
+bstReg_gpu = {}
+for sp in ['H2O2']:
+    for state in {'b:','u:'}:
+        mask = ~mask
+        target=np.log(df_y.loc[mask][sp])
+        dtrain = xgb.DMatrix(df_x.loc[mask], label=target)
+        param = {
+            # 'ntree_limit':200,
+            'max_depth': 15,
+            'eta': 0.3,
+            'silent': 1,
+            'predictor': 'gpu_predictor',
+            'objective': 'gpu:reg:linear'
+        }
 
-    }
+        num_round = 100
+        bst = xgb.train(param, dtrain, num_round)
+        # bst = xgb.cv(param, dtrain, num_round, nfold=5,
+        #    metrics={'error'}, seed=0,
+        #    callbacks=[xgb.callback.print_evaluation(show_stdv=True)])
 
-    num_round = 100
-    bst = xgb.train(param, dtrain, num_round)
+        bstReg_gpu[state+sp] = bst
 
-    bstReg_gpu[sp] = bst
+        print(sp + ':', r2_score(np.exp(bst.predict(dtrain)), target))
 
-    print(sp + ':', r2_score(np.exp(bst.predict(dtrain)), target))
-
-pred = sp_plot_gpu('T', bstReg_gpu, ode_o, ode_n)
+# pred = sp_plot_gpu('T', bstReg_gpu, ode_o, ode_n)
 # xgb.plot_importance(bst)
 # plt.plot(np.ma.masked_where((ode_o['H2']<0.01)|(ode_o['O2']<0.01),pred),'rd')
 # plt.show()
-mask_pred = (ode_o['T']<0.01)|(ode_o['O2']<0.01)
-mask_pred = ~mask_pred
-sp_plot_gpu_mask('T', bstReg_gpu, ode_o, ode_n,mask_pred)
+mask_pred = (ode_o['H2']<0.01)|(ode_o['O2']<0.01)
+
+sp_plot_gpu_mask('H2O2', bstReg_gpu, ode_o, ode_n,mask_pred)
 plt.show()
